441edbd0cb202fb1102425b038566f1df993d921
brown-qs <james@wubur.com>
2021-11-12 19:37:22 +0900

    password reset mail template update(2)

--- resources/views/emails/password_reset.blade.php
+++ resources/views/emails/password_reset.blade.php
@@ -12,29 +12,31 @@
     {{-- Intro Lines --}}
     @lang("You are receiving this email because we received a password reset request for your account.")
 
-    {{-- Action Button --}}
-    {{-- @component('mail::button', ['url' => $url, 'color' => 'primary'])
+    <a href="{{ $url }}" class="button button-primary" target="_blank" rel="noopener">
         @lang("Reset Password")
-    @endcomponent --}}
+    </a>
 
     {{-- Outro Lines --}}
     @lang("This link will expire in 60 minutes.")
     @lang("If you did not request a password reset, no further action is required.")
-
-    @component('mail::subcopy')
-        @lang(
-        "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-        'into your web browser:',
-        [
-        'actionText' => "Reset Password",
-        ]
-        ) <span class="break-all">[{{ $url }}]({{ $url }})</span>
-    @endcomponent
+    <table class="wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+        <tr>
+            <td>
+                @lang(
+                "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
+                'into your web browser:',
+                [
+                'actionText' => "Reset Password",
+                ]
+                ) <span class="break-all">[{{ $url }}]({{ $url }})</span>
+            </td>
+        </tr>
+    </table>
 
     {{-- Footer --}}
     @slot('footer')
         @component('mail::footer')
-            @lang("This message has been sent automatically. Please do not reply.") <br/>
+            @lang("This message has been sent automatically. Please do not reply.") <br />
             © {{ date('Y') }} {{ config('app.name') }}
         @endcomponent
     @endslot
