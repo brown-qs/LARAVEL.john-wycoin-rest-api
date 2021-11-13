004f0c5b2c3626f6a6125fb1f737e685dc6a3294
brown-qs <james@wubur.com>
2021-11-13 10:01:12 +0900

    Email template update

--- resources/views/emails/password_reset.blade.php
+++ resources/views/emails/password_reset.blade.php
@@ -25,7 +25,7 @@
 @lang("WyCoin Team")
 <table class="wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation">
 <tr>
-<td style="padding: 4rem">
+<td style="padding: 1rem">
 @lang(
 "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
 'into your web browser:',
@@ -42,7 +42,7 @@
 {{-- Footer --}}
 @slot('footer')
 @component('mail::footer')
-<p>@lang("This message has been sent automatically. Please do not reply.")</p>
+<p style="color:black">@lang("This message has been sent automatically. Please do not reply.")</p>
 © {{ date('Y') }} {{ config('app.name') }}
 @endcomponent
 @endslot
