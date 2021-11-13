f733542975d28b1d5030199fa783008068c29637
brown-qs <james@wubur.com>
2021-11-13 09:35:45 +0900

    Make Red Expire text on email template

--- resources/views/emails/password_reset.blade.php
+++ resources/views/emails/password_reset.blade.php
@@ -18,7 +18,7 @@
 @endcomponent
 
 {{-- Outro Lines --}}
-@lang("This link will expire in 60 minutes.")
+<p class="text-danger">@lang("This link will expire in 60 minutes.")</p>
 @lang("If you did not request a password reset, no further action is required.")
 
 @component('mail::subcopy')
