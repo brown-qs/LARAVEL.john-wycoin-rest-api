9fb9a1e7da391dfb78719584163606d78730002b
brown-qs <james@wubur.com>
2021-11-13 09:25:00 +0900

    Removed Indent in email template

--- resources/views/emails/password_reset.blade.php
+++ resources/views/emails/password_reset.blade.php
@@ -1,43 +1,42 @@
 @component('mail::layout')
-    {{-- Header --}}
-    @slot('header')
-        @component('mail::header', ['url' => config('app.url')])
-            <img src="{{ asset('img/logo.png') }}" width="200">
-        @endcomponent
-    @endslot
+{{-- Header --}}
+@slot('header')
+@component('mail::header', ['url' => config('app.url')])
+<img src="{{ asset('img/logo.png') }}" width="200">
+@endcomponent
+@endslot
 
-    {{-- Greeting --}}
-    @lang('Welcome!')
+{{-- Greeting --}}
+@lang('Welcome!')
 
-    {{-- Intro Lines --}}
-    @lang("You are receiving this email because we received a password reset request for your account.")
+{{-- Intro Lines --}}
+@lang("You are receiving this email because we received a password reset request for your account.")
 
-    <a href="{{ $url }}" class="button button-primary" target="_blank" rel="noopener">
-        @lang("Reset Password")
-    </a>
+{{-- Action Button --}}
+@component('mail::button', ['url' => $url, 'color' => 'primary'])
+@lang("Reset Password")
+@endcomponent
 
-    {{-- Outro Lines --}}
-    @lang("This link will expire in 60 minutes.")
-    @lang("If you did not request a password reset, no further action is required.")
-    <table class="wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation">
-        <tr>
-            <td>
-                @lang(
-                "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
-                'into your web browser:',
-                [
-                'actionText' => "Reset Password",
-                ]
-                ) <span class="break-all">[{{ $url }}]({{ $url }})</span>
-            </td>
-        </tr>
-    </table>
+{{-- Outro Lines --}}
+@lang("This link will expire in 60 minutes.")
+@lang("If you did not request a password reset, no further action is required.")
 
-    {{-- Footer --}}
-    @slot('footer')
-        @component('mail::footer')
-            @lang("This message has been sent automatically. Please do not reply.") <br />
-            © {{ date('Y') }} {{ config('app.name') }}
-        @endcomponent
-    @endslot
+@component('mail::subcopy')
+@lang(
+"If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\n".
+'into your web browser:',
+[
+'actionText' => "Reset Password",
+]
+) 
+<span class="break-all">[{{ $url }}]({{ $url }})</span>
+@endcomponent
+
+{{-- Footer --}}
+@slot('footer')
+@component('mail::footer')
+@lang("This message has been sent automatically. Please do not reply.") <br/>
+© {{ date('Y') }} {{ config('app.name') }}
+@endcomponent
+@endslot
 @endcomponent
