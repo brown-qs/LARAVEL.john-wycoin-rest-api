64dcfead26a03469ec997f923ccabd3a3df423ef
brown-qs <james@wubur.com>
2021-11-26 03:14:48 +0900

    coinbase to backend

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PortfolioController;
+
+use Illuminate\Support\Facades\Http;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -27,4 +30,21 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('add-portfolio', [PortfolioController::class, 'addPortfolio']);
     Route::get('get-portfolios', [PortfolioController::class, 'getPortfolios']);
     Route::get('load-portfolio-transactions/{id}', [PortfolioController::class, 'loadPortfolioTransactions']);
+    Route::post('coinbase-auth-token/{code}', function ($code) {
+        $client_id =
+            "cbe15651c9f49ef21ad8d08d8343764a7b772e3859cf309a015e8c4bd428e770";
+        $client_secret =
+            "f61c22fd167213f43fd1d5cccaa3b7a29247c7622f607920a20450e90165b5ad";
+        $redirect_uri = "http://localhost:3000/coinbase-oauth-redirect";
+
+
+        $response = Http::post('https://api.coinbase.com/oauth/token', [
+            'grant_type' => "authorization_code",
+            'code' => $code,
+            'client_id' => $client_id,
+            'client_secret' => $client_secret,
+            'redirect_uri' => $redirect_uri,
+        ]);
+        return $response->json();
+    });
 });
